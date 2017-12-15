0eca42434f9170afdd6655293d7cb85963ff73d6
Cenk Bircanoglu <cbircanoglu@boynergroup.com>
2017-12-15 22:35:38 +0300

    dnn updated

--- algo/dnn.py
+++ algo/dnn.py
@@ -1,146 +1,139 @@
 import sys
-import time
+from keras.callbacks import ModelCheckpoint
+from sklearn.model_selection import StratifiedKFold
 
 sys.path.insert(0, '../')
 
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, BatchNormalization, Dropout
 from keras.wrappers.scikit_learn import KerasRegressor
 from sklearn.metrics import mean_squared_error
-from keras import regularizers
+from keras import regularizers, callbacks
+
+import logging
+import sys
+import time
+
 import numpy as np
 import pandas as pd
-from scipy.special import boxcox1p
-from scipy.stats import skew
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler, LabelEncoder, Imputer, Normalizer, PolynomialFeatures, RobustScaler
+
+logger = logging.getLogger(__file__)
+hdlr = logging.FileHandler('train_dnn.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.WARNING)
+
+sys.path.insert(0, '../')
 
 train_data = pd.read_csv("../data/train.csv", index_col=False)
+print(train_data.shape)
 test_data = pd.read_csv("../data/test.csv", index_col=False)
+print(test_data.shape)
 
 tr_id = train_data["Id"]
 te_id = test_data["Id"]
-tr_y = np.log1p(train_data["SalePrice"])
-# te_y = test_data["SalePrice"]
+# tr_Y = np.log1p(train_data["SalePrice"])
+tr_Y = train_data["SalePrice"]
 
 train_data.drop("Id", axis=1, inplace=True)
 test_data.drop("Id", axis=1, inplace=True)
-
-distinct_values = {}
-for column in train_data.columns:
-    distinct_values[column] = train_data[column].nunique()
-print(distinct_values)
 
 ntrain = train_data.shape[0]
 ntest = test_data.shape[0]
 data = pd.concat((train_data, test_data)).reset_index(drop=True)
 data.drop(['SalePrice'], axis=1, inplace=True)
 
-data_na = (data.isnull().sum() / len(data)) * 100
-data_na = data_na.drop(data_na[data_na == 0].index).sort_values(ascending=False)
-missing_data = pd.DataFrame({'Missing Ratio': data_na})
-missing_data.plot(kind="barh", figsize=(6, 8))
+columns = ['Alley', 'BldgType', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2', 'BsmtQual', 'CentralAir',
+           'Condition1', 'Condition2', 'Electrical', 'ExterCond', 'ExterQual', 'Exterior1st', 'Exterior2nd', 'Fence',
+           'FireplaceQu', 'Foundation', 'Functional', 'GarageCond', 'GarageFinish', 'GarageQual',
+           'GarageType', 'Heating', 'HeatingQC', 'HouseStyle', 'KitchenQual', 'LandContour', 'LandSlope', 'LotConfig',
+           'LotShape', 'MSZoning', 'MasVnrType', 'MiscFeature', 'Neighborhood', 'PavedDrive', 'PoolQC',
+           'RoofMatl', 'RoofStyle', 'SaleCondition', 'SaleType', 'Street', 'Utilities']
 
-data["PoolQC"] = data["PoolQC"].fillna("None")
-data["MiscFeature"] = data["MiscFeature"].fillna("None")
-data["Alley"] = data["Alley"].fillna("None")
-data["Fence"] = data["Fence"].fillna("None")
-data["FireplaceQu"] = data["FireplaceQu"].fillna("None")
-data["LotFrontage"] = data.groupby("Neighborhood")["LotFrontage"].transform(lambda x: x.fillna(x.median()))
-for col in ('GarageType', 'GarageFinish', 'GarageQual', 'GarageCond'):
-    data[col] = data[col].fillna('None')
-for col in ('GarageYrBlt', 'GarageArea', 'GarageCars'):
-    data[col] = data[col].fillna(0)
-for col in ('BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', 'BsmtFullBath', 'BsmtHalfBath'):
-    data[col] = data[col].fillna(0)
-for col in ('BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2'):
-    data[col] = data[col].fillna('None')
-data["MasVnrType"] = data["MasVnrType"].fillna("None")
-data["MasVnrArea"] = data["MasVnrArea"].fillna(0)
-data['MSZoning'] = data['MSZoning'].fillna(data['MSZoning'].mode()[0])
-data = data.drop(['Utilities'], axis=1)
-data["Functional"] = data["Functional"].fillna("Typ")
-data['Electrical'] = data['Electrical'].fillna(data['Electrical'].mode()[0])
-data['KitchenQual'] = data['KitchenQual'].fillna(data['KitchenQual'].mode()[0])
-data['Exterior1st'] = data['Exterior1st'].fillna(data['Exterior1st'].mode()[0])
-data['Exterior2nd'] = data['Exterior2nd'].fillna(data['Exterior2nd'].mode()[0])
-data['SaleType'] = data['SaleType'].fillna(data['SaleType'].mode()[0])
-data['MSSubClass'] = data['MSSubClass'].fillna("None")
-data['TotalSF'] = data['TotalBsmtSF'] + data['1stFlrSF'] + data['2ndFlrSF']
-# Check remaining missing values if any
-data_na = (data.isnull().sum() / len(data)) * 100
-data_na = data_na.drop(data_na[data_na == 0].index).sort_values(ascending=False)
+for col in list(data):
+    if data[col].dtype == np.int64:
+        data[col] = Imputer().fit_transform(data[[col]])
+    elif data[col].dtype != object:
+        data[col] = data[col].mean()
 
-# MSSubClass=The building class
-data['MSSubClass'] = data['MSSubClass'].apply(str)
+for col in columns:
+    data[col] = data[col].fillna("None")
+    data[col] = LabelEncoder().fit_transform(data[col])
 
-# Changing OverallCond into a categorical variable
-data['OverallCond'] = data['OverallCond'].astype(str)
+data = StandardScaler().fit_transform(data)
+data = Normalizer().fit_transform(data)
+# data = PolynomialFeatures().fit_transform(data)
+print(data.shape)
+data = RobustScaler().fit_transform(data)
 
-# Year and month sold are transformed into categorical features.
-data['YrSold'] = data['YrSold'].astype(str)
-data['MoSold'] = data['MoSold'].astype(str)
-
-cols = ('FireplaceQu', 'BsmtQual', 'BsmtCond', 'GarageQual', 'GarageCond',
-        'ExterQual', 'ExterCond', 'HeatingQC', 'PoolQC', 'KitchenQual', 'BsmtFinType1',
-        'BsmtFinType2', 'Functional', 'Fence', 'BsmtExposure', 'GarageFinish', 'LandSlope',
-        'LotShape', 'PavedDrive', 'Street', 'Alley', 'CentralAir', 'MSSubClass', 'OverallCond',
-        'YrSold', 'MoSold')
-# process columns, apply LabelEncoder to categorical features
-for c in cols:
-    lbl = LabelEncoder()
-    lbl.fit(list(data[c].values))
-    data[c] = lbl.transform(list(data[c].values))
-print('Shape all_data: {}'.format(data.shape))
-
-numeric_feats = data.dtypes[data.dtypes != "object"].index
-
-skewed_feats = data[numeric_feats].apply(lambda x: skew(x.dropna())).sort_values(ascending=False)
-print("\nSkew in numerical features: \n")
-skewness = pd.DataFrame({'Skew': skewed_feats})
-
-skewness = skewness[abs(skewness) > 0.75]
-print("There are {} skewed numerical features to Box Cox transform".format(skewness.shape[0]))
-
-skewed_features = skewness.index
-lam = 0.15
-for feat in skewed_features:
-    data[feat] = boxcox1p(data[feat], lam)
-
-dummy_data = pd.get_dummies(data)
-tr_df = dummy_data[:ntrain]
-te_df = dummy_data[ntrain:]
-
-tr_X = tr_df.values
-te_X = te_df.values
+tr_X = data[:ntrain]
+te_X = data[ntrain:]
+print(tr_X.shape)
 
 
 # define wider model
 def wider_model():
     # create model
     model = Sequential()
-    model.add(Dense(256, input_dim=221, kernel_initializer='normal', activation='selu',
-                    kernel_regularizer=regularizers.l2(0.00001),
-                    activity_regularizer=regularizers.l1(0.00001)))
-    model.add(Dense(64, kernel_initializer='normal', activation='selu'))
-    model.add(Dense(64, kernel_initializer='normal', activation='selu'))
-    model.add(Dense(32, kernel_initializer='normal', activation='selu'))
-    model.add(Dense(32, kernel_initializer='normal', activation='selu'))
-    model.add(Dense(16, kernel_initializer='normal', activation='selu'))
-    model.add(Dense(16, kernel_initializer='normal', activation='selu'))
-    model.add(Dense(8, kernel_initializer='normal', activation='selu'))
-    model.add(Dense(4, kernel_initializer='normal', activation='selu'))
-    model.add(Dense(1))
+    model.add(Dense(256, input_dim=79, kernel_initializer='normal', activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dense(64, kernel_initializer='normal', activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dense(32, kernel_initializer='normal', activation='relu'))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.25))
+    model.add(Dense(1, kernel_initializer='normal'))
     # Compile model
-    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.compile(loss='mean_squared_logarithmic_error', optimizer='adam',
+                  metrics=["mean_squared_logarithmic_error", "mse"])
     return model
 
 
-estimator = KerasRegressor(build_fn=wider_model, epochs=250, batch_size=64, verbose=1)
-estimator.fit(tr_X, tr_y.values)
+def get_best_model(model, X, Y, train_indices, test_indeces, i):
+    tr_X = X[train_indices]
+    tr_Y = Y[train_indices]
+    te_X = X[test_indeces]
+    te_Y = Y[test_indeces]
+    best_weights_filepath = 'best_weights_%s.hdf5' % i
+    earlyStopping = callbacks.EarlyStopping(monitor='val_loss', patience=100, verbose=1, mode='auto')
+    saveBestModel = callbacks.ModelCheckpoint(best_weights_filepath, monitor='val_loss', verbose=1, save_best_only=True,
+                                              mode='auto')
+    mcp = ModelCheckpoint('best_checkpoint_%s.hdf5' % i, monitor="val_loss",
+                          save_best_only=True, save_weights_only=False)
 
-score = mean_squared_error(tr_y.values, estimator.predict(tr_X))
-print("\n", score)
+    # train model
+    history = model.fit(tr_X, tr_Y, batch_size=32, epochs=10000,
+                        verbose=1, validation_data=(te_X, te_Y), callbacks=[
+            earlyStopping,
+            saveBestModel,
+            mcp
+        ])
 
-te_y = estimator.predict(te_X)
+    # reload best weights
+    model.load_weights(best_weights_filepath)
+    return model
+
+
+n_folds = 10
+kfold = StratifiedKFold(n_splits=n_folds, shuffle=True, random_state=8)
+counter = 0
+best_models = []
+for train, test in kfold.split(tr_X, tr_Y):
+    model = wider_model()
+    best_model = get_best_model(model, tr_X, tr_Y, train, test, counter)
+    counter += 1
+    best_models.append(best_model)
+
+for best_model in best_models:
+    score = best_model.evaluate(tr_X, tr_Y)
+    print("\n", score)
+
+# te_y = np.expm1(best_model.predict(te_X))
+te_y = np.zeros((te_X.shape[0], 1))
+for best_model in best_models:
+    te_y += best_model.predict(te_X)
+te_y /= n_folds
 res = pd.DataFrame({"Id": te_id, "SalePrice": te_y.reshape(te_y.shape[0])})
 res.to_csv("../results/dnn_%s.csv" % time.time(), index=False)
